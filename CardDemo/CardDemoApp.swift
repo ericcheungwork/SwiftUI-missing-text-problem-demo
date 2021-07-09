e2e91a577ca0e9c6726ed488362442b10c28c9e9
Eric Cheung <ericcheungwork@gmail.com>
2021-07-09 15:47:53 +0800

    Initial Commit

---
+++ CardDemo/CardDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardDemoApp.swift
+//  CardDemo
+//
+//  Created by Eric Cheung on 9/7/2021.
+//
+
+import SwiftUI
+
+@main
+struct CardDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
