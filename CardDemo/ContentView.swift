aca4eecd084ea003846a68cb89eb10081454dde9
Eric Cheung <ericcheungwork@gmail.com>
2021-07-12 14:50:14 +0800

    Updated demo

--- CardDemo/ContentView.swift
+++ CardDemo/ContentView.swift
@@ -20,10 +20,11 @@ struct ContentView: View {
                 }
             }
             
-            if userData.currentSubtask != nil {
                 SubtaskCard()
                     .padding(.all)
-            }
+                    .frame(width: 300.0)
+                    .disabled(!(userData.currentSubtask != nil))
+
         }
         .onAppear {
                         
